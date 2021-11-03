1fccb94e0b562acedfe9c2c85b323c6a5930bf49
junki park <pjk146195@gmail.com>
2021-11-03 22:23:58 +0900

    Vuetify day01

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<!-- component UI definition-->
+<template>
+  <v-app>
+    <v-navigation-drawer app color="secondary" dark v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> Application </v-list-item-title>
+          <v-list-item-subtitle> with Vue + Vuetify </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item to="/">
+          <v-list-item-icon>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item to="/about">
+          <v-list-item-icon>
+            <v-icon>mdi-help-circle</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="secondary" dark>
+      <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Collapsing Bar</v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+    <v-footer app color="secondary" dark>
+      <div class="mx-auto">J.K.P</div>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  // component's name found in vue devtools on chrome.
+  name: "App",
+  // components that i want to add
+  components: {},
+  // component data definition
+  data: function () {
+    return {
+      drawer: false
+
+    };
+  },
+  // component method definition
+  methods: {},
+};
+</script>
+
+<!-- component style definition-->
+<style scoped></style>
