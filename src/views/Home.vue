086c930d89fedc349a7cc9d3d08cc8b48d6c6c19
junki park <pjk146195@gmail.com>
2021-11-08 12:13:29 +0900

    PWA 적용

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,18 @@
 <template>
-  <hello-world />
+  <div>
+    <hello-world />
+    <v-divider/>
+    <img src="@/assets/photos/photo1.jpg" height="150"/>
+    <img src="@/assets/photos/photo2.jpg" height="150"/>
+    <img src="@/assets/photos/photo3.jpg" height="150"/>
+    <img src="@/assets/photos/photo4.jpg" height="150"/>
+    <img src="@/assets/photos/photo5.jpg" height="150"/>
+    <img src="@/assets/photos/photo6.jpg" height="150"/>
+    <img src="@/assets/photos/photo7.jpg" height="150"/>
+    <img src="@/assets/photos/photo8.jpg" height="150"/>
+    <img src="@/assets/photos/photo9.jpg" height="150"/>
+    <img src="@/assets/photos/photo10.jpg" height="150"/>
+  </div>
 </template>
 
 <script>
